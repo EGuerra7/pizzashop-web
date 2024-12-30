6726fc7cfe1dcefb16e90083d98d0f77ea7f8fec
EGuerra7 <erickmuzzi@gmail.com>
2024-12-30 17:43:07 -0300

    App finalizado sem testes por enquanto!

---
+++ src/api/get-daily-revenue-in-period.ts
@@ -0,0 +1,28 @@
+import { api } from '@/lib/axios'
+
+export interface GetDaileRevenueInPeriodQuery {
+  from?: Date
+  to?: Date
+}
+
+export type GetDailyRevenueInPeriodResponse = {
+  date: string
+  receipt: number
+}[]
+
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDaileRevenueInPeriodQuery) {
+  const response = await api.get<GetDailyRevenueInPeriodResponse>(
+    '/metrics/daily-receipt-in-period',
+    {
+      params: {
+        from,
+        to,
+      },
+    },
+  )
+
+  return response.data
+}
--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -1,8 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
 
+import { getMonthRevenueAmount } from '@/api/get-month-revenue'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
+import { MetricCardSkeleton } from './metric-card-skeleton'
+
 export function MonthRevenueCard() {
+  const { data: monthReveneuAmount } = useQuery({
+    queryFn: getMonthRevenueAmount,
+    queryKey: ['metrics', 'month-reveneu'],
+  })
   return (
     <Card>
       <CardHeader className="flex-row items-center space-y-0 justify-between pb-2">
@@ -12,11 +20,35 @@ export function MonthRevenueCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">R$ 1248,60</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+2%</span> em
-          relação ao mês passado
-        </p>
+        {monthReveneuAmount ? (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {(monthReveneuAmount.receipt / 100).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthReveneuAmount.diffFromLastMonth >= 0 ? (
+                <>
+                  <span className="text-emarald-500 dark:text-emerald-400">
+                    +{monthReveneuAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  relação ao mês passado
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {monthReveneuAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  relação ao mês passado
+                </>
+              )}
+            </p>
+          </>
+        ) : (
+          <MetricCardSkeleton />
+        )}
       </CardContent>
     </Card>
   )
